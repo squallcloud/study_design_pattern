130d916f231300f826230bffc0ce8de9feded167
squallcloud <squallcloudzidane@gmail.com>
2020-12-31 01:15:54 +0900

    TECHSCOREの実習課題1の回答追加

--- Command001/Ex1/Beaker.cs
+++ Command001/Ex1/Beaker.cs
@@ -7,63 +7,69 @@ namespace Command001.Ex1
     //実験セット
     public class Beaker
     {
-        private double water = 0; //水
-        private double salt = 0; //食塩
-        private bool melted = true; //食塩がすべて溶けたときtrue、溶け残ったときfalse
+        private double m_water = 0; //水
+        private double m_salt = 0; //食塩
+        private bool m_melted = true; //食塩がすべて溶けたときtrue、溶け残ったときfalse
+
+        //濃度
+        public double Density => m_salt / (m_water + m_salt) * 100;
 
         //コンストラクタ
         public Beaker(double water, double salt)
         {
-            this.water = water;
-            this.salt = salt;
-            this.mix();//かき混ぜる
+            m_water = water;
+            m_salt = salt;
+
+            //かき混ぜる
+            mix();
         }
+
         //ビーカーに食塩を入れるメソッド
         public void addSalt(double salt)
         {
-            this.salt += salt;
+            m_salt += salt;
         }
+
         //ビーカーに水を入れるメソッド
         public void addWater(double water)
         {
-            this.water += water;
+            m_water += water;
         }
         //かき混ぜるメソッド
         public void mix()
         {
+            const double kLimit = 26.4;
+
             //溶液をかき混ぜる
             //溶けたか溶け残ったかをセットする
             //常温での飽和食塩水の濃度は約26.4%
-            if ((this.salt / (this.salt + this.water)) * 100 < 26.4)
-            {
-                melted = true;
-            }
-            else
-            {
-                melted = false;
+            if (Density < kLimit) {
+                m_melted = true;
+            } else {
+                m_melted = false;
             }
         }
         //食塩の量を返すメソッド
         public double getSalt()
         {
-            return salt;
+            return m_salt;
         }
         //水の量を返すメソッド
         public double getWater()
         {
-            return water;
+            return m_water;
         }
         //溶けたか溶け残ったか調べるメソッド
         public bool isMelted()
         {
-            return melted;
+            return m_melted;
         }
         //実験結果をノートに記録する
         public void note()
         {
-            Console.WriteLine("水：" + water + "g");
-            Console.WriteLine("食塩：" + salt + "g");
-            Console.WriteLine("濃度：" + (salt / (water + salt)) * 100 + "%");
+            Console.WriteLine($"水：{m_water}g");
+            Console.WriteLine($"食塩：{m_salt}g");
+            Console.WriteLine($"濃度：{Density}%");
         }
     }
 }
--- Command001/Ex1/Student.cs
+++ Command001/Ex1/Student.cs
@@ -10,19 +10,19 @@ namespace Command001.Ex1
         public void Execute()
         {
             //実験内容(コマンドオブジェクト)を用意する
-            Command addSalt = new AddSaltCommand();
-            Command addWater = new AddWaterCommand();
             Command makeSaltWater = new MakeSaltWaterCommand();
-
+            Command addSalt = new AddSaltCommand();
+            MakeSaltWaterAddSaltCommand makeSaltWaterAddSalt = new MakeSaltWaterAddSaltCommand();
             //実験セットを実験内容にセットする
-            addSalt.setBeaker(new Beaker(100, 0));//水100gの入ったビーカーをセットする
-            addWater.setBeaker(new Beaker(0, 10));//食塩10gの入ったビーカーをセットする
-            makeSaltWater.setBeaker(new Beaker(90, 10));//水90g、食塩10gの入ったビーカーをセットする
-
+            Beaker beaker = new Beaker(90, 10);
+            makeSaltWater.setBeaker(beaker);
+            addSalt.setBeaker(beaker);
+            makeSaltWaterAddSalt.setBeaker(beaker);
+            //実験リストをセットする
+            makeSaltWaterAddSalt.addCommand(makeSaltWater);
+            makeSaltWaterAddSalt.addCommand(addSalt);
             //実験を行う
-            addSalt.execute(); //食塩を加えて飽和食塩水を作る実験
-            addWater.execute(); //水を加えて飽和食塩水を作る実験
-            makeSaltWater.execute(); //10%の食塩水100gを作る実験
+            makeSaltWaterAddSalt.execute();
         }
     }
 }
