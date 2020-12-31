339c08a03d86b065e74579704b8509296768ef0e
squallcloud <squallcloudzidane@gmail.com>
2020-12-31 15:40:38 +0900

    機能：TemplateMethodパターンのサンプルコード追加

--- TemplateMethod001/Program.cs
+++ TemplateMethod001/Program.cs
@@ -6,7 +6,8 @@ namespace TemplateMethod001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new Example.Client();
+            client.Execute();
         }
     }
 }
